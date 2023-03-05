4734d68d4d5d8f2d06035632dc8f7b5c9cf0a8c6
JulianRB <ignis.lykaios@gmail.com>
2023-03-04 19:00:28 -0600

    Agregado componente Pagination como función paginate a utils. Aún no se renderiza.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import Proyect from '../Proyect/Proyect';
+import { paginate } from './../../utils/paginate';
+import Pagination from '../Pagination/Pagination';
 
 export default function Main(props) {
   const { proyects, openPopupWithConfirmation, setSelectedProyect } = props;
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const pageSize = 3;
+
+  const paginatedProyects = paginate(proyects, currentPage, pageSize);
+  const totalCount = paginatedProyects.length;
+
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
 
   if (proyects.length >= 1)
     return (
@@ -19,6 +31,12 @@ export default function Main(props) {
           );
         })}
         <Link to="/proyect/create">Agregar</Link>
+        <Pagination
+          itemsCount={totalCount}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
       </section>
     );
 }
