fe8bbffe8ee07a61d06a49b44fb43c01655683b7
JulianRB <ignis.lykaios@gmail.com>
2023-03-14 13:49:45 -0600

    Correcciones para eliminar errores eslint

--- src/components/BackgroundImg/BackgroundImg.jsx
+++ src/components/BackgroundImg/BackgroundImg.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BackgroundImg(props) {
   return (
     <div className="background-img__overlay">
-      <img src={props.src} alt="Background image" className="background-img" />
+      <img src={props.src} alt="Background" className="background-img" />
     </div>
   );
 }
